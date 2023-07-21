a05f7ce5c1627d2a8b592c2e4c0c0ecc4dc7252e
临风 <linfeng@jravity.com>
2023-07-21 20:36:35 +0800

    add readme file

---
+++ build/CMakeFiles/kde-wine-helper.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kde-wine-helper_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/kde-wine-helper_autogen.dir/ParseCache.txt"
+  "kde-wine-helper_autogen"
+  "CMakeFiles/kde-wine-helper.dir/Notifier.cpp.o"
+  "CMakeFiles/kde-wine-helper.dir/Notifier.cpp.o.d"
+  "CMakeFiles/kde-wine-helper.dir/XWindowListener.cpp.o"
+  "CMakeFiles/kde-wine-helper.dir/XWindowListener.cpp.o.d"
+  "CMakeFiles/kde-wine-helper.dir/kde-wine-helper_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/kde-wine-helper.dir/kde-wine-helper_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/kde-wine-helper.dir/main.cpp.o"
+  "CMakeFiles/kde-wine-helper.dir/main.cpp.o.d"
+  "kde-wine-helper"
+  "kde-wine-helper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kde-wine-helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/kde-wine-helper_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kde-wine-helper_autogen"
+  "kde-wine-helper_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/kde-wine-helper_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
